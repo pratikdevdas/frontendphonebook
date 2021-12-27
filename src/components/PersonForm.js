1227aecabbb057d1eca4ceec47a3e4f060ebb36e
rev <dasdev.pratik@gmail.com>
2021-12-27 09:47:12 +0530

    create with token forntend

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -1,5 +1,7 @@
 import React from 'react'
-const PersonForm = ({prop1,prop2,prop3,prop4,prop5}) =>{return (<><form onSubmit={prop1} >
+const PersonForm = ({prop1,prop2,prop3,prop4,prop5}) =>{
+  return (
+  <><form onSubmit={prop1}>
     <div>
       name: <input value={prop2} onChange={prop3}/>
     </div>
